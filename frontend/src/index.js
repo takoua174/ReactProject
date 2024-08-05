469cd196e03c6db1db730ebb6b1a4963b7ebb2ce
takoua174 <taki74ayadi@gmail.com>
2024-08-05 01:32:54 +0100

    ExpenseItem

---
+++ frontend/src/index.js
@@ -0,0 +1,8 @@
+//this is the entry point of the application
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
